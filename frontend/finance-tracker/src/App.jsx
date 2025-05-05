001a80da7cfdae0721883f6b3ae51cd2bb0cad48
hemanialaparthi <alaparthi01@allegheny.edu>
2025-05-04 20:27:11 -0400

    feat(App.jsx): add routing and basic authentication for the app

--- frontend/finance-tracker/src/App.jsx
+++ frontend/finance-tracker/src/App.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
+
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root/>} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App
+
+export const Root = () => {
+  // check if token is present in local storage
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  // then, redirect to the dashboard if authenticates, if not, redirect to the login page
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
